c965963e73e5b622c106d034e9163551e040f799
jah <empty>
2000-01-16 16:22:07 +0000

    DIC.asm: *** empty log message ***

--- DIC.asm
+++ DIC.asm
@@ -4,7 +4,7 @@
 ;  :Author.	Bert Jahn
 ;  :EMail.	wepl@kagi.com
 ;  :Address.	Franz-Liszt-Straße 16, Rudolstadt, 07404, Germany
-;  :Version.	$Id: error.i 1.2 1998/12/06 13:42:20 jah Exp $
+;  :Version.	$Id: DIC.asm 0.17 1999/01/17 14:17:38 jah Exp jah $
 ;  :History.	15.05.96
 ;		20.06.96 returncode supp.
 ;		01.06.97 _LVOWaitForChar added,  check for interactive terminal added
@@ -12,8 +12,10 @@
 ;			 if last disk already reached now (reported by MrLarmer)
 ;		12.10.98 bugfix
 ;		17.01.99 recompile because error.i changed
+;		11.01.00 pedantic mode and skipping tracks added
+;			 error handling changed
 ;  :Requires.	OS V37+
-;  :Copyright.	© 1996,1997,1998 Bert Jahn, All Rights Reserved
+;  :Copyright.	© 1996,1997,1998,1999,2000 Bert Jahn, All Rights Reserved
 ;  :Language.	68000 Assembler
 ;  :Translator.	Barfly V2.9
 ;  :To Do.
@@ -37,18 +39,18 @@
 	STRUCTURE	Globals,0
 		APTR	gl_execbase
 		APTR	gl_dosbase
+		ULONG	gl_stdin
 		APTR	gl_rdargs
 		LABEL	gl_rdarray
 		ULONG	gl_rd_device
+		ULONG	gl_rd_st
 		ULONG	gl_rd_size
 		ULONG	gl_rd_fdisk
 		ULONG	gl_rd_ldisk
+		ULONG	gl_rd_pedantic
 		ULONG	gl_rc
 		ALIGNLONG
 		LABEL	gl_SIZEOF
-
-BREAKCOUNT	= 1000
-MAXFILENAMESIZE	= 30	;31 with the termination zero
 
 ;##########################################################################
 
@@ -57,7 +59,7 @@ LOC	EQUR	A5		;a5 for local vars
 CPU	=	68000
 
 Version	 = 0
-Revision = 17
+Revision = 18
 
 	PURE
 	OUTPUT	C:DIC
@@ -76,6 +78,7 @@ VER	MACRO
 		dc.b	0,"$VER: "
 		VER
 		dc.b	0
+		dc.b	"$Id$",10,0
 	EVEN
 .start
 
@@ -89,7 +92,7 @@ VER	MACRO
 		beq	.nostrucmem
 		move.l	d0,GL
 		move.l	a6,(gl_execbase,GL)
-		move.l	#20,(gl_rc,GL)
+		move.l	#RETURN_FAIL,(gl_rc,GL)
 
 		move.l	#37,d0
 		lea	(_dosname),a1
@@ -148,6 +151,11 @@ VER	MACRO
 		move.l	(a0),d1
 .2		move.l	d1,(gl_rd_ldisk,GL)
 
+		move.l	(gl_dosbase,GL),a6
+		jsr	(_LVOInput,a6)
+		move.l	d0,(gl_stdin,GL)
+
+		move.l	#RETURN_ERROR,(gl_rc,GL)
 		bsr	_Main
 .badargs
 		move.l	(gl_rdargs,GL),d1
@@ -168,7 +176,7 @@ VER	MACRO
 		move.l	d7,d0
 		rts
 
-.nostrucmem	moveq	#20,d0
+.nostrucmem	moveq	#RETURN_FAIL,d0
 		rts
 
 ;##########################################################################
@@ -180,6 +188,7 @@ VER	MACRO
 		Print
 		FlushOutput
 		CheckBreak
+		GetKey
 	INCLUDE	files.i
 		SaveFileMsg
 	INCLUDE	strings.i
@@ -199,22 +208,20 @@ _Main		move.l	(gl_rd_device,GL),d1
 		move.l	(gl_dosbase,GL),a6
 		jsr	(_LVOInhibit,a6)
 
-		move.l	(gl_dosbase,GL),a6
-		jsr	(_LVOInput,a6)
-		move.l	d0,d6			;d6 = stdin
-
 		move.l	(gl_rd_fdisk,GL),d7	;d7 disknumber
 		subq.l	#1,d7
 .nextdisk	addq.l	#1,d7
 		cmp.l	(gl_rd_ldisk,GL),d7
-		bhi	.end
+		bhi	.success
 
-.again		move.l	d6,d1
+	;check if interactive console
+		move.l	(gl_stdin,GL),d1
 		move.l	(gl_dosbase,GL),a6
 		jsr	(_LVOIsInteractive,a6)
 		tst.l	d0
 		beq	.readdisk
 
+	;prompt user to insert disk
 		lea	(_insdisk),a0
 		move.l	(gl_rd_device,GL),-(a7)
 		move.l	d7,-(a7)
@@ -222,58 +229,29 @@ _Main		move.l	(gl_rd_device,GL),d1
 		bsr	_PrintArgs
 		addq.l	#8,a7
 		bsr	_FlushOutput
-		
-		move.l	d6,d1
+		move.l	(gl_stdin,GL),d1
 		move.l	(gl_dosbase,GL),a6
 		jsr	(_LVOFlush,a6)
 		
-.wait		move.l	d6,d1
-		move.l	#100*1000,d2
-		jsr	(_LVOWaitForChar,a6)
-		tst.l	d0
-		bne	.getc
-		bsr	_CheckBreak
-		tst.l	d0
-		beq	.wait
-		bra	.end
-		
-.getc		move.l	d6,d1
-		jsr	(_LVOFGetC,a6)
+	;wait for return
+.wait		bsr	_GetKey
+		cmp.b	#3,d0			;^C ?
+		beq	.end
+		cmp.b	#13,d0			;CR ?
+		bne	.wait
+		bsr	_PrintLn
 
+	;read the disk image
 .readdisk	move.l	(gl_rd_device,GL),a0
 		bsr	_LoadDisk
 		move.l	d1,d4			;D4 = size
 		move.l	d0,d5			;D5 = buffer
-		bne	.save
-		
-		move.l	d6,d1
-		move.l	(gl_dosbase,GL),a6
-		jsr	(_LVOIsInteractive,a6)
-		tst.l	d0
 		beq	.end
-
-		lea	(_tryagain),a0
-		bsr	_Print
-		bsr	_FlushOutput
-		move.l	d6,d1
-		move.l	(gl_dosbase,GL),a6
-		jsr	(_LVOFlush,a6)
-		move.l	d6,d1
-		jsr	(_LVOFGetC,a6)
-		cmp.b	#"a",d0
-		blo	.4
-		cmp.b	#"z",d0
-		bhi	.4
-		sub.b	#$20,d0
-.4		cmp.b	#"C",d0
-		beq	.nextdisk
-		cmp.b	#"R",d0
-		beq	.again
-		bra	.end
-
+		
 NAMEBUFLEN = 16
 
-.save		lea	(_filefmt),a0		;fmt
+	;save disk image
+		lea	(_filefmt),a0		;fmt
 		move.l	d7,-(a7)
 		move.l	a7,a1			;args
 		sub.l	#NAMEBUFLEN,a7
@@ -290,18 +268,18 @@ NAMEBUFLEN = 16
 		move.l	a2,a1			;filename
 		bsr	_SaveFileMsg
 		add.l	#NAMEBUFLEN+4,a7
-		move.l	d0,d2
+		move.l	d0,d2			;save return code
 		move.l	d5,a1
 		move.l	(gl_execbase,GL),a6
 		jsr	(_LVOFreeVec,a6)
 		tst.l	d2
-		beq	.end
+		beq	.end			;if save file has failed
 
-		cmp.l	(gl_rd_ldisk,GL),d7
-		bne	.nextdisk
-		clr.l	(gl_rc,GL)
-.end
-		move.l	(gl_rd_device,GL),d1
+		bra	.nextdisk
+
+.success	clr.l	(gl_rc,GL)
+
+.end		move.l	(gl_rd_device,GL),d1
 		moveq	#0,d2
 		move.l	(gl_dosbase,GL),a6
 		jsr	(_LVOInhibit,a6)
@@ -310,21 +288,22 @@ NAMEBUFLEN = 16
 
 ;##########################################################################
 ;----------------------------------------
-; Lesen einer Diskette
-; Übergabe :	A0 = CPTR device name
-; Rückgabe :	D0 = APTR loaded buffer OR NIL
-;		D1 = LONG size of buffer
+; read disk
+; IN:	A0 = CPTR device name
+; OUT:	D0 = APTR loaded buffer OR NIL
+;	D1 = LONG size of buffer
 
 	NSTRUCTURE	local_dodisk,0
-		NSTRUCT	ld_di,devi_SIZEOF	;DeviceInfo
-		NSTRUCT	ld_devname,DEVNAMELEN	;devicename without ":"
+		NSTRUCT	ld_di,devi_SIZEOF		;DeviceInfo
+		NSTRUCT	ld_devname,DEVNAMELEN		;devicename without ":"
 		NLABEL	ld_SIZEOF
 
 _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		link	LOC,#ld_SIZEOF
-		moveq	#0,d7			;D7 = returncode (bufptr)
-		moveq	#0,d6			;D6 = bufsize
+		moveq	#0,d7				;D7 = returncode (bufptr)
+		moveq	#0,d6				;D6 = bufsize
 		
+	;remove ":" from device name
 		lea	(ld_devname,LOC),a1
 		moveq	#DEVNAMELEN-1,d0
 .c		move.b	(a0)+,(a1)+
@@ -335,12 +314,14 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		bne	.1
 		clr.b	(a1)
 .1
+	;get geometry for device
 		lea	(ld_devname,LOC),a0
 		lea	(ld_di,LOC),a1
 		bsr	_GetDeviceInfo
 		tst.l	d0
 		beq	.nodevi
 
+	;print device name
 		lea	(_m_readdisk),a0
 		move.l	(ld_di+devi_Unit,LOC),-(a7)
 		pea	(ld_di+devi_Device,LOC)
@@ -354,7 +335,7 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		mulu.l	(ld_di+devi_Surfaces,LOC),d2
 	ELSE
 		move.l	(ld_di+devi_Surfaces,LOC),d0
-		mulu32	d0,d2					;D2 = starttrack
+		mulu32	d0,d2					;D2 = first track
 	ENDC
 
 		move.l	(ld_di+devi_HighCyl,LOC),d3
@@ -364,7 +345,7 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		mulu.l	(ld_di+devi_Surfaces,LOC),d3
 	ELSE
 		move.l	(ld_di+devi_Surfaces,LOC),d0
-		mulu32	d0,d3					;D3 = tracks
+		mulu32	d0,d3					;D3 = amount of tracks
 	ENDC
 
 	IFEQ CPU-68020
@@ -376,9 +357,10 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		mulu32	d0,d4					;D4 = tracksize
 	ENDC
 
+	;print geometry
 		lea	(_m_diskgeo),a0
 		move.l	d3,d6
-		mulu32	d4,d6		;disksize = cyls * heads * blktrk * blksize
+		mulu32	d4,d6			;disksize = cyls * heads * blktrk * blksize
 		move.l	d6,-(a7)
 		move.l	(ld_di+devi_HighCyl,LOC),-(a7)
 		move.l	(ld_di+devi_LowCyl,LOC),-(a7)
@@ -396,8 +378,8 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		bls	.sok
 		moveq	#0,d3
 		moveq	#0,d6
-.add		addq.l	#1,d3				;D3 = tracks
-		add.l	d4,d6				;D6 = readsize
+.add		addq.l	#1,d3					;D3 = amount of tracks
+		add.l	d4,d6					;D6 = bufsize
 		cmp.l	(gl_rd_size,GL),d6
 		blo	.add
 .sok
@@ -414,9 +396,9 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 		bra	.nomem
 .memok
 		lea	(ld_di+devi_Device,LOC),a0
-		move.l	a0,d0				;D0 = devicename
-		move.l	(ld_di+devi_Unit,LOC),d1	;D1 = unit
-		move.l	d7,a1				;A1 = buffer
+		move.l	a0,d0					;D0 = devicename
+		move.l	(ld_di+devi_Unit,LOC),d1		;D1 = unit
+		move.l	d7,a1					;A1 = buffer
 		bsr	_ReadDisk
 		tst.l	d0
 		bne	.ok
@@ -437,20 +419,22 @@ _LoadDisk	movem.l	d2-d7/a6,-(a7)
 
 ;----------------------------------------
 ; Lesen Diskette
-; Übergabe :	D0 = APTR  device name
-;		D1 = ULONG unit number
-;		D2 = ULONG start track
-;		D3 = ULONG tracks
-;		D4 = ULONG bytes per track
-;		A1 = APTR  buffer to read data in
-;		GL = STRUCT globals
-; Rückgabe :	D0 = BOOL success
+; IN:	D0 = APTR  device name
+;	D1 = ULONG unit number
+;	D2 = ULONG start track
+;	D3 = ULONG tracks
+;	D4 = ULONG bytes per track
+;	A1 = APTR  buffer to read data in
+;	GL = STRUCT globals
+; OUT:	D0 = BOOL success
 
 	NSTRUCTURE	local_readdisk,0
 		NAPTR	lrd_device
 		NULONG	lrd_unit
 		NAPTR	lrd_buffer
 		NAPTR	lrd_msgport
+		NBYTE	lrd_skipall
+		NALIGNLONG
 		LABEL	lrd_SIZEOF
 
 _ReadDisk	movem.l	d2-d3/d7/a2/a6,-(a7)
@@ -458,9 +442,10 @@ _ReadDisk	movem.l	d2-d3/d7/a2/a6,-(a7)
 		move.l	d0,(lrd_device,LOC)
 		move.l	d1,(lrd_unit,LOC)
 		move.l	a1,(lrd_buffer,LOC)
-		moveq	#-1,d7			;D7 = return (true)
+		sf	(lrd_skipall,LOC)
+		moveq	#0,d7				;D7 = return (false)
 
-		move.l	(gl_execbase,GL),a6	;A6 = execbase !!!
+		move.l	(gl_execbase,GL),a6		;A6 = execbase
 		jsr	(_LVOCreateMsgPort,a6)
 		move.l	d0,(lrd_msgport,LOC)
 		bne	.portok
@@ -468,42 +453,42 @@ _ReadDisk	movem.l	d2-d3/d7/a2/a6,-(a7)
 		lea	(_noport),a0
 		sub.l	a1,a1
 		bsr	_PrintError
-		moveq	#0,d7
 		bra	.noport
 .portok		
 		move.l	(lrd_msgport,LOC),a0
 		move.l	#IOTD_SIZE,d0
 		jsr	(_LVOCreateIORequest,a6)
-		move.l	d0,a2
+		move.l	d0,a2				;A2 = ioreq
 		tst.l	d0
 		bne	.ioreqok
 		moveq	#0,d0
 		lea	(_noioreq),a0
 		sub.l	a1,a1
 		bsr	_PrintError
-		moveq	#0,d7
 		bra	.noioreq
 .ioreqok
 		move.l	(lrd_device,LOC),a0
 		move.l	(lrd_unit,LOC),d0
-		move.l	a2,a1			;ioreq
-		move.l	#0,d1			;flags
-	move.l	d3,-(a7)			;BUG in fucking mfm.device
+		move.l	a2,a1				;ioreq
+		move.l	#0,d1				;flags
+	move.l	d3,-(a7)				;BUG in fucking mfm.device
 		jsr	(_LVOOpenDevice,a6)
-	move.l	(a7)+,d3			;BUG in fucking mfm.device
+	move.l	(a7)+,d3				;BUG in fucking mfm.device
 		tst.l	d0
 		beq	.deviceok
 		move.b	(IO_ERROR,a2),d0
 		lea	(_opendevice),a0
 		bsr	_PrintErrorTD
-		moveq	#0,d7
 		bra	.nodevice
 .deviceok
-		move.l	a2,a1
+
+	;get actual diskchange count
+		move.l	a2,a1				;ioreq
 		move.w	#TD_CHANGENUM,(IO_COMMAND,a1)
 		jsr	(_LVODoIO,a6)
-		move.l	(IO_ACTUAL,a2),(IOTD_COUNT,a2)	;the diskchanges
+		move.l	(IO_ACTUAL,a2),(IOTD_COUNT,a2)
 
+	;setup the ioreq
 		move.l	(lrd_buffer,LOC),(IO_DATA,a2)	;dest buf
 		move.l	d2,d0
 		mulu32	d4,d0
@@ -511,7 +496,7 @@ _ReadDisk	movem.l	d2-d3/d7/a2/a6,-(a7)
 		move.l	d4,(IO_LENGTH,a2)		;bytes per track
 		move.w	#ETD_READ,(IO_COMMAND,a2)
 		
-		add.l	d2,d3				;d3 lasttrack
+		add.l	d2,d3				;D3 = last track
 
 		bsr	_PrintLn
 .loop		lea	(_diskprogress),a0		;output progress
@@ -523,40 +508,106 @@ _ReadDisk	movem.l	d2-d3/d7/a2/a6,-(a7)
 		bsr	_PrintArgs
 		addq.l	#8,a7
 		
-		bsr	_CheckBreak			;check for CTRL-C
+	;check for CTRL-C
+		bsr	_CheckBreak
 		tst.l	d0
-		bne	.readbreak
+		bne	.break
 		
+	;check if track should be skipped
+		move.l	(gl_rd_st,GL),d0
+		beq	.nost
+		move.l	d0,a0
+		cmp.l	(a0),d2
+		beq	.skip
+.nost
+	;read the track
 		move.l	a2,a1				;read one track
+		clr.b	(IO_ERROR,a2)
+		move.l	(gl_execbase,GL),a6
 		jsr	(_LVODoIO,a6)
 		move.b	(IO_ERROR,a2),d0
-		bne	.readerr
-.readfurther	addq.l	#1,d2
-		cmp.l	d2,d3
 		beq	.readok
-		add.l	d4,(IO_OFFSET,a2)		;begin at disk (offset)
-		add.l	d4,(IO_DATA,a2)			;dest buf
-		bra	.loop
-
-.readerr	lea	(_readdisk),a0
+		lea	(_readdisk),a0
 		bsr	_PrintErrorTD
-		cmp.b	#TDERR_DiskChanged,(IO_ERROR,a2)
-		beq	.readbreak
-	;	tst.l	(gl_rd_ignoreerrors,GL)
-	;	beq	.readbreak
-		bsr	_PrintLn
-		move.l	(IO_DATA,a2),a0			;fill unreadable area with "WRIP"
+
+		tst.l	(gl_rd_pedantic,GL)
+		bne	.break
+		
+		tst.b	(lrd_skipall,LOC)
+		bne	.skip
+
+		move.l	(gl_stdin,GL),d1
+		move.l	(gl_dosbase,GL),a6
+		jsr	(_LVOIsInteractive,a6)
+		tst.l	d0
+		beq	.skip
+
+		lea	(_tryagain),a0
+		bsr	_Print
+		bsr	_FlushOutput
+		
+.wait		bsr	_GetKey
+		cmp.b	#3,d0				;Ctrl-C
+		beq	.break
+		UPPER	d0
+		cmp.b	#"R",d0
+		beq	.waitend
+		cmp.b	#"S",d0
+		beq	.waitend
+		cmp.b	#"A",d0
+		beq	.waitend
+		cmp.b	#"Q",d0
+		beq	.waitend
+		cmp.b	#13,d0				;Return
+		bne	.wait
+		
+.waitend	lsl.w	#8,d0
+		or.w	#10,d0
+		clr.w	-(a7)
+		move.w	d0,-(a7)
+		move.l	a7,a0
+		bsr	_Print
+		move.l	(a7)+,d0
+		rol.l	#8,d0
+		cmp.b	#"R",d0
+		beq	.loop
+		cmp.b	#"S",d0
+		beq	.skip
+		cmp.b	#"Q",d0
+		beq	.break
+		cmp.b	#13,d0				;Return
+		beq	.break
+
+	;skip all errors
+		st	(lrd_skipall,LOC)
+
+.skip
+	;fill track data area with pattern "TDIC"
+		move.l	(IO_DATA,a2),a0
 		move.l	(IO_LENGTH,a2),d0
 		lsr.l	#2,d0
 .fill		move.l	#"TDIC",(a0)+
 		subq.l	#1,d0
 		bne	.fill
-		bra	.readfurther
-.readbreak	moveq	#0,d7
-.readok
+		
+		bsr	_PrintLn
+
+	;next track
+.readok		add.l	d4,(IO_OFFSET,a2)		;begin at disk (offset)
+		add.l	d4,(IO_DATA,a2)			;dest buf
+		addq.l	#1,d2
+		cmp.l	d2,d3
+		bne	.loop
+
+		bsr	_PrintLn
+		moveq	#-1,d7
+.break
+
+	;switch drive motor off
 		move.l	a2,a1
 		move.l	#0,(IO_LENGTH,a1)
 		move.w	#ETD_MOTOR,(IO_COMMAND,a1)
+		move.l	(gl_execbase,GL),a6
 		jsr	(_LVODoIO,a6)
 
 		move.l	a2,a1
@@ -568,19 +619,16 @@ _ReadDisk	movem.l	d2-d3/d7/a2/a6,-(a7)
 .noioreq	move.l	(lrd_msgport,LOC),a0
 		jsr	(_LVODeleteMsgPort,a6)
 		
-.noport		move.l	d7,d0
+.noport		move.l	d7,d0				;return code
 		unlk	LOC
 		movem.l	(a7)+,d2-d3/d7/a2/a6
 		rts
 
 ;##########################################################################
 
-
-;##########################################################################
-
 _defdev		dc.b	"DF0:",0
-_insdisk	dc.b	10,"Insert disk %ld in drive %s and press RETURN (^C to cancel) ...",0
-_tryagain	dc.b	"Retry/Cancel/Quit (r/c/Q) : ",0
+_insdisk	dc.b	10,"Insert disk %ld into drive %s and press RETURN (^C to cancel) ...",0
+_tryagain	dc.b	"Retry/Skip/skip All/Quit (r/s/a/Q): ",0
 _filefmt	dc.b	"Disk.%ld",0
 
 ;Messages
@@ -588,7 +636,7 @@ _m_readdisk	dc.b	"read from ",155,"1m%s",155,"22m: (%s %ld)",10,0
 _m_diskgeo	dc.b	"(blksize=%ld heads=%ld blktrk=%ld lcyl=%ld hcyl=%ld) size=%ld",10,0
 _m_savedisk	dc.b	"save disk as ",155,"3m%s ",155,"23m",10,0
 _m_savefile	dc.b	"save file ",155,"3m%s ",155,"23m",10,0
-_diskprogress	dc.b	11,"reading track %ld left %ld  ",10,0
+_diskprogress	dc.b	11,155,"Kreading track %ld left %ld",10,0
 _withsize	dc.b	"limited reading of $%lx=%ld bytes",10,0
 
 ; Errors
@@ -610,9 +658,11 @@ _opendevice	dc.b	"open device",0
 _dosname	DOSNAME
 
 _template	dc.b	"DEVICE"		;name of device (default "DF0:)
+		dc.b	",SKIPTRACK/K/N"	;dont read these tracks
 		dc.b	",SIZE/K"		;number of bytes
 		dc.b	",FD=FIRSTDISK/K/N"	;number of first disk
 		dc.b	",LD=LASTDISK/K/N"	;number of last disk
+		dc.b	",PEDANTIC/S"		;quit on unreadable tracks
 		dc.b	0
 
 _ver		VER
