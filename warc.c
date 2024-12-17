dcff99fdb0040827375beb995465501ce197b239
Bert Jahn <wepl@whdload.de>
2024-12-17 21:13:46 +0100

    warc: archive/unarchive lha works

--- warc.c
+++ warc.c
@@ -1,6 +1,16 @@
 /*
- *  transform WHDLoad data directories into VFS-archives
- *  decompress XPK packed files and call lha/zip to create archive
+ * transform WHDLoad data directories into VFS-archives and vice versa
+ * decompress XPK packed files and call lha/zip to create archive
+ * or the reverse direction (unarchive and restore data directories)
+ *
+ * if Src is a file:
+ *	.info -> un/archive data directories of this icon/slave and update icon
+ *	.lha/zip -> unarchive to directory of filename and delete archive
+ *	else failing
+ * if Src is a directory:
+ *	pack this directory into an archive of this name and delete directory
+ *	if Scan/S is set search this directory for WHDLoad icons and create archives
+ *	if Scan/S and UnArc/S is set search this directory for WHDLoad icons and unarchive
  *
  * 2021-01-20 started
  * 2021-07-14 lha working without XPK files
@@ -9,6 +19,8 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #include <dos/dos.h>
 #include <xpk/xpk.h>
@@ -16,19 +28,26 @@
 #include <proto/exec.h>
 #include <proto/xpkmaster.h>
 
-#define TEMPLATE "Src/A,Remove/S,TmpDir,Verbose/S"
+#define TEMPLATE "Src/A,Scan/S,UnArc/S,NoDelete/S,TmpDir/K,Verbose/S"
 #define OPT_SRC		0
-#define OPT_REMOVE	1
-#define OPT_TMPDIR	2
-#define OPT_VERBOSE	3
-#define OPT_COUNT	4
+#define OPT_SCAN	1
+#define OPT_UNARC	2
+#define OPT_NODELETE	3
+#define OPT_TMPDIR	4
+#define OPT_VERBOSE	5
+#define OPT_COUNT	6
 
 LONG opts[OPT_COUNT];
-STRPTR ext = "lha";
-const char * defcmdlha = "lha -eFrZ3 -Qw a";	// default pack command for lha archives
+const char * extlha = ".lha";
+const char * extzip = ".zip";
+const char * extinfo = ".info";
+const char * defcmdlha = "lha -eFrYZ3 -Qw a";	// default pack command for lha archives
+const char * defxcmdlha = "lha -CF -Qa x";	// default unpack command for lha archives
+const char * defcmdzip = "zip -RND";	// default pack command for lha archives
+const char * defxcmdzip = "unzip";	// default unpack command for zip archives
 const char * deftmpdir = "T:warc.tmp";	// default directory to store decompressed files
-BPTR fhunc, fhdec;			// fhs for temporary list files
-int cntdir, cntfile, cntxpk;		// counters directories, files, xpk-files
+BPTR fhunc, fhdec;			// filehandles for temporary list files
+int cntdir, cntfile, cntxpk;		// counters for directories, files, xpk-files
 int cntsz, cntszxpk, cntszarc;		// size of all files uncompressed, size saved by xpk, saved by archive
 struct Library *XpkBase;		// xpkmaster.library
 
@@ -46,7 +65,7 @@ void doserr(const char *op, const char *obj) {
 }
 
 /*
- * print verbose message
+ * print verbose message with added newline
  * in:
  * 	msg	format string
  * 	...	args
@@ -57,6 +76,7 @@ void verbose(const char *msg, ...) {
 		va_list args;
 		va_start(args, msg);
 		vprintf(msg, args);
+		printf("\n");
 	}
 }
 
@@ -65,10 +85,14 @@ void verbose(const char *msg, ...) {
  * checks if the file is completely compressed to avoid false
  * detections on disk images for example
  * Warning: file may be encrypted/compressed multiple times!
- * returns: 0=not-compressed 1=compresed 2=error
+ * in:
+ *	name	file name
+ *	size	file size
+ * out:
+ *	0=not-compressed 1=compressed 2=error
  */
 int checkxpk(const char *name, ULONG size) {
-	ULONG buf[32];
+	ULONG buf[32/4];		// XPK header size required to check
 	BPTR fh;
 	// check for min filesize
 	if (size <= sizeof(buf)) return 0;
@@ -85,7 +109,7 @@ int checkxpk(const char *name, ULONG size) {
 	}
 	Close(fh);
 	if (buf[0] == 'X'<<24|'P'<<16|'K'<<8|'F' && buf[1] == size-8) {
-		verbose("checkxpk: %s packed=%lu unpacked=%lu\n",name,size,buf[3]);
+		verbose("checkxpk: '%s' packed=%lu unpacked=%lu",name,size,buf[3]);
 		return 1;
 	}
 	return 0;
@@ -101,7 +125,8 @@ int checkxpk(const char *name, ULONG size) {
  * 	tmpdir	directory lock to store the decompressed file, if NULL overwrite source
  * 	fib	FileInfoBlock of file to decompress
  * 	size	variable to return uncompressed size
- * returns: 0=error 1=success
+ * out:
+ *	0=error 1=success
  */
 int unpackxpk(BPTR tmpdir, struct FileInfoBlock *fib, ULONG *outlen) {
 	ULONG *in, *out;
@@ -111,12 +136,14 @@ int unpackxpk(BPTR tmpdir, struct FileInfoBlock *fib, ULONG *outlen) {
 	LONG err;
 	char errhead[] = "XPK-unpack failed";
 	BPTR fh, olddir;
-	
+
+	verbose("unpackxpk: '%s'", fib->fib_FileName);
+
 	// open the XPK library
 	if (!XpkBase) {
 		XpkBase = OpenLibrary (xpkname, xpkver);
 		if (!XpkBase) {
-			printf("cannot open %s version %d\n", xpkname, xpkver);
+			fprintf(stderr ,"cannot open %s version %d\n", xpkname, xpkver);
 			return 0;
 		}
 	}
@@ -163,12 +190,12 @@ int unpackxpk(BPTR tmpdir, struct FileInfoBlock *fib, ULONG *outlen) {
 
 	// write unpacked file
 	err = 0;
-	fh = Open(fib->fib_FileName,MODE_NEWFILE);
+	fh = Open(fib->fib_FileName, MODE_NEWFILE);
 	if (!fh) {
-		doserr("open to write",fib->fib_FileName);
+		doserr("open to write", fib->fib_FileName);
 	} else {
-		if (*outlen != Write(fh,out,*outlen)) {
-			doserr("write",fib->fib_FileName);
+		if (*outlen != Write(fh, out, *outlen)) {
+			doserr("write", fib->fib_FileName);
 		} else {
 			err = 1;
 		}
@@ -200,12 +227,14 @@ int unpackxpk(BPTR tmpdir, struct FileInfoBlock *fib, ULONG *outlen) {
 
 /*
  * create parent dir of the given path
+ * works recursive
  * in:
  * 	path	path
- * returns: 0=error 1=success
+ * out:
+ *	0=error 1=success
  */
 int createdirparent(const char *path) {
-	char * part, save;
+	char * part, savedchar;
 	BPTR lock;
 
 	part = PathPart(path);
@@ -213,7 +242,7 @@ int createdirparent(const char *path) {
 		doserr("pathpart",path);		// to get at least a message
 		return 0;
 	}
-	save = *part;
+	savedchar = *part;
 	*part = 0;
 	if (! (lock = CreateDir(path))) {
 		if (IoErr() != ERROR_DIR_NOT_FOUND) {
@@ -221,7 +250,6 @@ int createdirparent(const char *path) {
 			return 0;
 		}
 		if (! createdirparent(path)) {
-			// error message done by gettmpdirparent
 			return 0;
 		}
 		// try again
@@ -231,7 +259,7 @@ int createdirparent(const char *path) {
 		}
 	}
 	UnLock(lock);
-	*part = save;
+	*part = savedchar;
 	return 1;
 }
 
@@ -240,11 +268,14 @@ int createdirparent(const char *path) {
  * if directories above are missing create them also
  * in:
  * 	path	path of the dir to create
- * returns: 0=error lock-to-dir
+ * out:
+ *	0=error lock-to-dir
  */
 BPTR gettmpdir(const char *path) {
 	char buf[256];
 	BPTR lock;
+
+	verbose("gettmpdir: '%s'", path);
 
 	snprintf(buf,sizeof(buf),(char*)opts[OPT_TMPDIR]);
 	AddPart(buf,path,sizeof(buf));
@@ -264,26 +295,45 @@ BPTR gettmpdir(const char *path) {
 }
 
 /*
- * recursive function to scan a directory
- * XPK files are decompressed
- * file names are written to fhunc/fhdec
+ * print string + new line to file
+ * in:
+ * 	fh	file handle
+ * 	text	string to print
+ * out:
+ *	0=error 1=success
+ */
+int putline(BPTR fh, const char *txt) {
+	if (FPuts(fh,txt) != 0 || FPutC(fh,'\n') != '\n') {
+		char buffer[256];
+		NameFromFH(fh, buffer, sizeof(buffer));
+		doserr("writing filelist", buffer);
+		return 0;
+	}
+	return 1;
+}
+
+/*
+ * recursive function to scan a directory for later compression
+ * XPK files are decompressed to directory tmpdir
+ * file names are written to filehandles fhunc/fhdec
  * in:
  * 	path	path name of the current dir
  * 	dir	directory name in the current dir to scan
- * returns: 0=error 1=success
+ * out:
+ *	0=error 1=success
  */
 int scan(const char *path, const char *dir) {
 	BPTR l, o, tmpdir=0;
 	struct FileInfoBlock fib;
-	int rc=0;
+	int rc=0;		// default = failed
 	char newpath[256], name[256];
 	ULONG size;
-	
+
 	// build new path
 	snprintf(newpath,sizeof(newpath),path);
 	AddPart(newpath,dir,sizeof(newpath));
 
-	// lock & change directory
+	// lock & change to directory
 	l = Lock(dir,SHARED_LOCK);
 	if (!l) {
 		doserr("lock",dir);
@@ -298,10 +348,11 @@ int scan(const char *path, const char *dir) {
 		while (ExNext(l,&fib)) {
 			snprintf(name,sizeof(name),newpath);
 			AddPart(name,fib.fib_FileName,sizeof(name));
+			// do we need special handling for links here?
 			if (fib.fib_DirEntryType >= 0) {
 				// directory
 				cntdir++;
-				verbose("dir: %s\n",name);
+				verbose("dir:  '%s'",name);
 				if (! scan(newpath,fib.fib_FileName)) goto failed;
 			} else {
 				// file
@@ -310,8 +361,8 @@ int scan(const char *path, const char *dir) {
 				switch (checkxpk(fib.fib_FileName,fib.fib_Size)) {
 					case 0:	// uncompressed
 						cntsz += fib.fib_Size;
-						verbose("file: %s size=%ld\n",name,fib.fib_Size);
-						FPuts(fhunc,name); FPutC(fhunc,'\n');
+						verbose("file: '%s' size=%ld",name,fib.fib_Size);
+						if (! putline(fhunc,name)) goto failed;
 						break;
 					case 1:	// compressed
 						cntxpk++;
@@ -319,8 +370,8 @@ int scan(const char *path, const char *dir) {
 						if (! unpackxpk(tmpdir,&fib,&size)) goto failed;
 						cntsz += size;
 						cntszxpk += size - fib.fib_Size;
-						verbose("filexpk: %s packed=%ld unpacked=%lu\n",name,fib.fib_Size,size);
-						FPuts(fhdec,name); FPutC(fhdec,'\n');
+						verbose("filexpk: '%s' packed=%ld unpacked=%lu",name,fib.fib_Size,size);
+						if (! putline(fhdec,name)) goto failed;
 						break;
 					case 2:	// error
 						goto failed;
@@ -347,17 +398,62 @@ int scan(const char *path, const char *dir) {
 }
 
 /*
+ * delete given directory recursively
+ * in:
+ * 	dir	directory name to delete
+ * out:
+ *	0=success >0=error
+ */
+int deleteDir(const STRPTR dirname) {
+	int rc;
+	char cmd[256];
+	snprintf(cmd,sizeof(cmd),"Delete \"%s\" Quiet Force All", dirname);
+	rc = SystemTagList(cmd, NULL);
+	if (rc != RETURN_OK) {
+		fprintf(stderr, "deleting directory failed: '%s'\n",cmd);
+	}
+	return rc;
+}
+
+/*
+ * delete given directory recursively only if NoDelete/S is not set
+ * in:
+ * 	dir	directory name to delete
+ * out:
+ *	0=success >0=error
+ */
+int deleteDirOpt(const STRPTR dirname) {
+	if (opts[OPT_NODELETE]) {
+		return RETURN_OK;
+	} else {
+		return deleteDir(dirname);
+	}
+}
+
+/*
+ * print and execute given command
+ * in:
+ * 	cmd	command to execute
+ * out:
+ *	return code of command
+ */
+int system(const STRPTR cmd) {
+	printf("executing '%s'\n",cmd);
+	return SystemTagList(cmd,NULL);
+}
+
+/*
  * create archive from the given directory
  * the directory must be located in the actual directory!
  * - scan the directory and decompress all xpk files to 'tmpdir'
- *   preserve file meta data
+ *   and preserve file meta data
  * - if there were no xpk files just archive the directory using lha/zip
  * - if there were xpk files:
  *   lha: create archive via filelist from actual directory and 'tmpdir'
  *   zip: create archive via filelist from actual directory and in a
- *        second add all files from 'tmpdir'
+ *        second run add all files from 'tmpdir'
  */
-int packDir(const STRPTR dirname) {
+int archiveDir(const STRPTR dirname) {
 	int rc = RETURN_ERROR;
 	BPTR dir, old, arc;
 	char arcname[256];
@@ -369,7 +465,7 @@ int packDir(const STRPTR dirname) {
 
 	// make sure the given directory is located in the current dir
 	if (FilePart(dirname) != dirname) {
-		printf("%s: must be directory in the current dir\n",dirname);
+		fprintf(stderr,"archiveDir: '%s' must be directory in the current dir\n",dirname);
 		return rc;
 	}
 
@@ -381,7 +477,7 @@ int packDir(const STRPTR dirname) {
 	}
 
 	// build archive name
-	snprintf(arcname,sizeof(arcname),"%s.%s",dirname,ext);
+	snprintf(arcname,sizeof(arcname),"%s%s",dirname,extlha);
 
 	// check if archive already exists
 	arc = Open(arcname,MODE_OLDFILE);
@@ -439,8 +535,7 @@ int packDir(const STRPTR dirname) {
 	if (cntxpk) {
 		snprintf(cmd+len,sizeof(cmd)-len," @%s",listdec);
 	}
-	printf("executing '%s'\n",cmd);
-	rc = SystemTagList(cmd,NULL);
+	rc = system(cmd);
 	if (rc != RETURN_OK) {
 		printf("archiving '%s' failed\n",arcname);
 		return rc;
@@ -464,17 +559,157 @@ int packDir(const STRPTR dirname) {
 
 	// delete unpacked XPK files
 	if (cntxpk) {
-		snprintf(cmd,sizeof(cmd),"Delete '%s' Quiet Force All",(char*)opts[OPT_TMPDIR]);
-		rc = SystemTagList(cmd,NULL);
-		if (rc != RETURN_OK) {
-			printf("deleting unpacked XPK files failed: '%s'\n",cmd);
-			return rc;
-		}
+		rc = deleteDir((char*)opts[OPT_TMPDIR]);
 	}
 
-	// delete source files if option REMOVE is active
-
 	return rc;
+}
+
+/*
+ * standard clib
+ */
+int strcasecmp(const char *s1, const char *s2) {
+	int offset,ch;
+	unsigned char a,b;
+
+	offset = 0;
+	ch = 0;
+	while( *(s1+offset) != 0 ) {
+		/* check for end of s2 */
+		if ( *(s2+offset)==0) return( *(s1+offset) );
+		a = (unsigned)*(s1+offset);
+		b = (unsigned)*(s2+offset);
+		ch = toupper(a) - toupper(b);
+		if ( ch<0 || ch>0 ) return(ch);
+		offset++;
+	}
+	return(ch);
+}
+
+/*
+ * unarchive file from the actual directory
+ * 	arcname	name of archive to extract
+ * out:
+ *	RETURN_OK, RETURN_ERROR
+ */
+int unarchive(const STRPTR arcname) {
+	verbose("unarchiving '%s'", arcname);
+	size_t namelen = strlen(arcname);
+	size_t extlen = strlen(extlha);
+	if (namelen <= extlen) return RETURN_ERROR;
+	size_t baselen = namelen - extlen;
+	const char *xcmd;
+	if (strcasecmp(arcname+baselen, extlha) == 0) {
+		xcmd = defxcmdlha;
+	} else if (strcasecmp(arcname+baselen, extzip) == 0) {
+		xcmd = defxcmdzip;
+	} else return RETURN_ERROR;
+	// create data directory to store archive content
+	char data[256];
+	strncpy(data, arcname, baselen);
+	data[baselen] = 0;
+	BPTR dir = CreateDir(data);
+	if (! dir) {
+		doserr("create dir", data);
+		return RETURN_ERROR;
+	}
+	// lock from CreateDir is an exclusive lock, SystemTagList cannot dup it
+	if (! ChangeMode(CHANGE_LOCK, dir, ACCESS_READ)) {
+		doserr("change mode lock", data);
+		UnLock(dir);
+		return RETURN_ERROR;
+	}
+	// enter directory
+	BPTR olddir = CurrentDir(dir);
+	// extract
+	char cmd[256];
+	snprintf(cmd, sizeof(cmd), "%s /%s", xcmd, arcname);
+	int rc = system(cmd);
+	// return directory
+	CurrentDir(olddir);
+	UnLock(dir);
+	// return
+	if (rc != RETURN_OK) {
+		fprintf(stderr, "unarchiving failed: '%s'\n",cmd);
+		deleteDir(data);
+	}
+	return rc;
+}
+
+/*
+ * amiga.lib
+ */
+void NewList(struct MinList *lh)
+{
+	lh->mlh_Head = (struct MinNode *)&(lh->mlh_Tail);
+	lh->mlh_Tail = NULL;
+	lh->mlh_TailPred = (struct MinNode *)&(lh->mlh_Head);
+}
+
+/*
+ * check if name has given extension
+ * in:
+ * 	name	name to check
+ * 	ext	extension to check
+ * out:
+ *	0=hasn't <>0=has
+ */
+int cmpExt(const STRPTR name, const STRPTR ext) {
+	size_t namelen = strlen(name);
+	size_t extlen = strlen(ext);
+	if (namelen > extlen && strcasecmp(name+namelen-extlen, ext) == 0) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+/*
+ * check if name has one of the supported archive extensions
+ * in:
+ * 	name	name to check
+ * out:
+ *	0=hasn't <>0=has
+ */
+int cmpExtArc(const STRPTR name) {
+	if (cmpExt(name, extlha) || cmpExt(name, extzip)) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+/*
+ * analyse given icon
+ * if DefaultTool is WHDLoad search ToolTypes
+ * if ToolType Data is found perform Un/Archive for all data directories not already processed
+ * else load Slave and check for slv_CurrentDir, perform Archive if exists
+ * processed data directories are remembered in datalist
+ * update/add icons Data ToolType if something has changed
+ * in:
+ * 	name	name of icon to check, must be located in the actual directory!
+ * 	list	list of already processed data directories
+ * out:
+ *	RETURN_OK, RETURN_ERROR
+ */
+int processIcon(const STRPTR name, struct MinList *list) {
+	verbose("processIcon: '%s'", name);
+	fprintf(stderr,"processIcon: not implemented\n");
+	return RETURN_ERROR;
+}
+
+/*
+ * scan given directory recursively for .info files
+ * process (un/archive) all found WHDLoad data directories
+ * in:
+ * 	dirname	name of directory to scan
+ * out:
+ *	RETURN_OK, RETURN_ERROR
+ */
+int scanDir(const STRPTR dirname) {
+	verbose("scanDir: '%s'", dirname);
+	fprintf(stderr,"scanDir: not implemented\n");
+	return RETURN_ERROR;
 }
 
 /*
@@ -489,7 +724,68 @@ int main (void) {
 	if (rdargs == NULL) {
 		PrintFault(IoErr(),NULL);
 	} else {
-		rc = packDir((STRPTR) opts[OPT_SRC]);
+		// check arguments
+		// TmpDir must not already exist
+		BPTR lock;
+		if ((lock = Lock((char*)opts[OPT_TMPDIR], SHARED_LOCK)) != 0) {
+			UnLock(lock);
+			fprintf(stderr, "error: TmpDir '%s' already exists!\n", (char*)opts[OPT_TMPDIR]);
+		} else {
+			// check if Src is directory or file
+			char *src = (char*) opts[OPT_SRC];
+			lock = Lock(src, SHARED_LOCK);
+			if (lock == 0) {
+				doserr("open", src);
+			} else {
+				struct FileInfoBlock fib;
+				if (! Examine(lock,&fib)) {
+					doserr("examine",src);
+					UnLock(lock);
+				} else {
+					// enter parent directory of Src required for file and archiveDir modes
+					BPTR parent = ParentDir(lock);
+					BPTR olddir = CurrentDir(parent);
+					// free the lock because the object may get deleted on success
+					UnLock(lock);
+					// check for dir/file
+					if (fib.fib_DirEntryType >= 0) {
+						// directory
+						// scan or archive
+						if (opts[OPT_SCAN]) {
+							// may not relative to current dir!
+							CurrentDir(olddir);
+							rc = scanDir(src);
+						} else {
+							rc = archiveDir(fib.fib_FileName);
+							if (rc == RETURN_OK) rc = deleteDirOpt(fib.fib_FileName);
+						}
+					} else {
+						// file
+						if (cmpExtArc(fib.fib_FileName)) {
+							rc = unarchive(fib.fib_FileName);
+							if (rc == RETURN_OK) {
+								if (! DeleteFile(fib.fib_FileName)) {
+									doserr("delete", fib.fib_FileName);
+									rc = RETURN_WARN;
+								}
+							}
+						} else if (cmpExt(fib.fib_FileName, extinfo)) {
+							struct MinList list;
+							NewList(&list);
+							rc = processIcon(fib.fib_FileName, &list);
+							struct MinNode *node;
+							for ( node = list.mlh_Head ; node->mln_Succ != NULL ; node = node->mln_Succ )
+								FreeVec(node);
+						} else {
+							fprintf(stderr, "error: invalid file '%s', must be archive or icon\n", src);
+						}
+					}
+					// return to initial directory
+					CurrentDir(olddir);
+					UnLock(parent);
+				}
+			}
+		}
 		FreeArgs(rdargs);
 	}
 	if (XpkBase) { CloseLibrary(XpkBase); }
